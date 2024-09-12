92788851e2e1c74902af50b61a8f2cfb7f06a2f4
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-09-13 01:16:44 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mortgage-calculator';
+  purchasePrice = 0;
+  downPayment = 0;
+  repayTime = 1;
+  interestRate = 0;
+
+  estimatedMonthlyPayment = 0;
+
+  getMortgage() {
+    const principal = this.purchasePrice - this.downPayment;
+    const monthlyInterestRate = this.interestRate / 100 / 12;
+    const totalPayments = this.repayTime * 12;
+  
+    if (monthlyInterestRate === 0) {
+      this.estimatedMonthlyPayment = principal / totalPayments;
+    } else {
+      this.estimatedMonthlyPayment =
+        principal *
+        (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments)) /
+        (Math.pow(1 + monthlyInterestRate, totalPayments) - 1);
+    }
+  }
 }
